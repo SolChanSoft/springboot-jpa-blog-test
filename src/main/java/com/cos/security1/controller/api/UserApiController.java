5119afe00216ef426c96056edda06018e60d8808
KangChiHwan <kcw4892@gmail.com>
2022-05-05 23:15:32 +0900

    로그인 가능하게 수정

--- src/main/java/com/cos/security1/controller/api/UserApiController.java
+++ src/main/java/com/cos/security1/controller/api/UserApiController.java
@@ -1,15 +1,27 @@
 package com.cos.security1.controller.api;
 
+import com.cos.security1.domain.RoleType;
+import com.cos.security1.domain.User;
 import com.cos.security1.dto.ResponseDto;
+import com.cos.security1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user")
     public ResponseDto<Integer> save(@RequestBody User user){
-       // System.out.println("UserApiController: save호출");
-        return new ResponseDto<Integer>(HttpStatus.OK, 1);
+        System.out.println("UserApiController: save호출");
+        // 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
+        user.setRole(RoleType.ROLE_USER.name());
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result); // 자바오브젝트를 JSON으로 변환해서 리턴(jackson)
     }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,12 +1,11 @@
 let index = {
-    init: function(){
-        $("#btn-save").on("click",()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서 사용
+    init : function(){
+        $("#btn-save").on("click", ()=>{ // function(){}, ()=>{} this를 바인딩하기 위해서 사용
             this.save();
         });
     },
 
     save: function (){
-        //alert('User의 Save함수 호출됨');
         let data = {
             username: $("#username").val(),
             password: $("#password").val(),
@@ -19,18 +18,18 @@ let index = {
         $.ajax({
             //회원가입 수행요청
             type: "POST",
-            url: "/api/user/join",
+            url: "/api/user",
             data: JSON.stringify(data), //http body데이터
             contentType: "application/json; charset=utf-8",  //body데이터가 어떤 타입인지(MIME)
             dataType: "json"  // 요청을 서버로해서 응답이 왔을때 기본적으로 모든것이 String(문자열)
                               // 생긴게 json이라면 -> javascript로 변환
         }).done(function(resp){
             alert("회원가입이 완료되었습니다.");
-            location.href = "/blog";
+            location.href = "/user";
         }).fail(function(){
-            alert(JSON.stringify(error));
+            alert(JSON.stringify(onerror));
         }); //ajax통신을 이용해서 3개의 데이터를 JSON으로 변경하여 Insert요청!!
-    }
+     }
 }
 
 index.init();
